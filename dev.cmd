75a0424a700396be1735502d7225ef3318f1a862
wisdomCX <1361791276@qq.com>
2021-03-14 18:00:02 +0800

    追加

--- dev.cmd
+++ dev.cmd
@@ -1,1 +1,20 @@
-node ./bin/index  %*
+@echo off
+SETLOCAL
+CALL :find_dp0
+
+IF EXIST "%dp0%\node.exe" (
+  SET "_prog=%dp0%\node.exe"
+  echo "------------"
+) ELSE (
+  SET "_prog=node"
+  SET PATHEXT=%PATHEXT:;.JS;=;%
+)
+echo %dp0%
+%_prog%   "%dp0%\bin\index.js" %*
+
+ENDLOCAL
+EXIT /b %errorlevel%
+:find_dp0
+SET dp0=%~dp0
+EXIT /b
+
